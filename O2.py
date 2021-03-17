de042779ff6b365593b57ab3e53d12947f519559
Richard <richard.bonfrere@gmail.com>
2021-03-17 11:38:04 +0100

    Deel 2 start

---
+++ O2.py
@@ -0,0 +1,13 @@
+# Opdracht 2
+
+Bord = [[0, 0, 0],[0, 0, 0],[0, 0, 0]]
+
+def FindNextStates(bord):
+    return bord
+
+def Move():
+    print("Move")
+
+def CheckWin(bord):
+    #controleer of er drie op een rij is gemaakt
+    return 0
