8fdb6039e383a4df3212c460ef20a400a4f1cabe
Richard <richard.bonfrere@gmail.com>
2021-04-08 17:22:43 +0200

    Taalherkenner: Import Teksten

---
+++ Inleveropdracht 3/Languagedetector.py
@@ -0,0 +1,7 @@
+# Stap 1: Trigrammen en bigrammen halen uit teksten
+# Zet deze in een tabel voor 6 talen met de kansen die daarbij horen
+# deze tabel ergens opslaan
+# nieuw python bestand, daarin de herkenner
+# de herkenner haalt uit de zin alle bigrammen en trigrammen
+# en vergelijkt deze met de kansen van de talen
+# de hoogste kans is de taal dat het is.
---
+++ Inleveropdracht 3/Languagetrainer.py
@@ -0,0 +1,9 @@
+# stap 1: haal alle documenten op
+de = open('DE.txt', 'r')
+DE = de.read()
+print(DE)
+# stap 2: haal uit ieder document alle bi en trigrammen en zet deze in een (turf)tabel
+
+# stap 3: maak van de turftabel een tabel met kansen
+
+# stap 4: sla deze tabel ergens permanent op
